3c6cb3a0abdd6ecde776b4c230028d62936c8c7a
Vishnu <lanetteam.vishnu@gmail.com>
2018-06-14 01:40:49 +0530

    add Img

--- src/Component/home.js
+++ src/Component/home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Pusher from 'pusher-js';
 import axios from 'axios';
+import u112Img from '../img/u112.jpg';
 
 export default class home extends Component
 {
@@ -43,22 +44,37 @@ export default class home extends Component
                     this.props.changeUser('');
                 }} value="Logout" />
                 <br /><br /><br />
-                <table style={{ marginLeft: 'auto', marginRight: 'auto', width: 180}}>
-                    <tr>
-                        <th>No</th>
-                        <th>Action</th>
-                    </tr>
-                    {
-                        this.state.arr.map((dat, index) =>
-                            <tr>
-                                <td>{dat.key}</td>
-                                <td>
-                                    <input type="button" onClick={() => this.handlePart(index, dat.key, dat.value)} style={{ color: dat.value?'green':'red'}} value={dat.value} />
-                                </td>
-                            </tr>)
-                    }
-                    {this.state.arr.length===0?<tr><td colSpan='2'><i>No connected Pi</i></td></tr>:""}
-                </table>
+
+                {this.state.arr.length == 4
+                    ?
+                    <div align="center" style={{ display: 'inline-block', position: 'relative' }}>
+                        <img src={u112Img} style={{position: 'relative', width: 400, height: 400}}/>
+                        <input type="button" value={this.state.arr[0].value} onClick={() => this.handlePart(0, this.state.arr[0].key, this.state.arr[0].value)} style={{position: 'absolute', top: '40%', left: '2%', color: this.state.arr[0].value ? 'green' : 'red' }}/>
+                        <input type="button" value={this.state.arr[1].value} onClick={() => this.handlePart(1, this.state.arr[1].key, this.state.arr[1].value)} style={{position: 'absolute', top: '50%', right: '2%', color: this.state.arr[1].value ? 'green' : 'red' }}/>
+                        <input type="button" value={this.state.arr[2].value} onClick={() => this.handlePart(2, this.state.arr[2].key, this.state.arr[2].value)} style={{position: 'absolute', bottom: '30%', left: '15%', color: this.state.arr[2].value ? 'green' : 'red' }}/>
+                        <input type="button" value={this.state.arr[3].value} onClick={() => this.handlePart(3, this.state.arr[3].key, this.state.arr[3].value)} style={{position: 'absolute', bottom: '4%', right: '20%', color: this.state.arr[3].value ? 'green' : 'red' }}/>
+                    </div>
+                    :
+                    <table style={{marginLeft: 'auto', marginRight: 'auto', width: 180}}>
+                        <tr>
+                            <th>No</th>
+                            <th>Action</th>
+                        </tr>
+                        {
+                            this.state.arr.map((dat, index) =>
+                                <tr>
+                                    <td>{dat.key}</td>
+                                    <td>
+                                        <input type="button" onClick={() => this.handlePart(index, dat.key, dat.value)}
+                                               style={{color: dat.value ? 'green' : 'red'}} value={dat.value}/>
+                                    </td>
+                                </tr>)
+                        }
+                        {this.state.arr.length === 0 ? <tr>
+                            <td colSpan='2'><i>No connected Pi</i></td>
+                        </tr> : ""}
+                    </table>
+                }
             </div>
         );
     }
